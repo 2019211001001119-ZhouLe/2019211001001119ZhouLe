020dd65a93244d2eaef205d62c5a4ca16843295e
Homosapiens0 <homosapien@foxmail.com>
2021-03-12 18:29:06 +0800

    2.HW-CE1 Commit

--- src/main/java/com/ZhouLe/week2/helloServlet.java
+++ src/main/java/com/ZhouLe/week2/helloServlet.java
@@ -5,13 +5,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class helloServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
         writer.println("Name : Zhou Le");
         writer.println("ID : 2019211001001119");
-        writer.println("Date and time : 2021/3/12 18:18");
+        writer.println("Date and time : " + formatter.format(date));
     }
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         PrintWriter writer = response.getWriter();
